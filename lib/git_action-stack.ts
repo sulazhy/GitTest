12275dc636fb661a69f3b1e7d4e74e64224102ad
Abdul <metalsquad4u@gmail.com>
2022-08-24 21:03:44 +0300

    first git action commit

--- lib/git_action-stack.ts
+++ lib/git_action-stack.ts
@@ -1,16 +1,35 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+
+import * as cdk from 'aws-cdk-lib';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 
 export class GitActionStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const key = new kms.Key(this, 'github-action-kms-key', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pendingWindow: cdk.Duration.days(7),
+      alias: 'alias/mykey',
+      description: 'KMS key for encrypting the objects in an S3 bucket',
+      enableKeyRotation: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GitActionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const s3Bucket = new s3.Bucket(this, 'my-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      encryption: s3.BucketEncryption.KMS,
+      // 👇 encrypt with our KMS key
+      encryptionKey: key,
+    });
+
+    new cdk.CfnOutput(this, 'key-arn', {
+      value: key.keyArn,
+    });
+
+    new cdk.CfnOutput(this, 'bucket-name', {
+      value: s3Bucket.bucketName,
+    });
+  } 
 }
