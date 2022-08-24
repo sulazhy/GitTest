59a745a1089e7e9197c595a31093846de017ffb2
Abdul <metalsquad4u@gmail.com>
2022-08-25 00:08:18 +0300

    second git action commit

--- lib/git_action-stack.ts
+++ lib/git_action-stack.ts
@@ -8,7 +8,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3'
 export class GitActionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
+/*
     const key = new kms.Key(this, 'github-action-kms-key', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       pendingWindow: cdk.Duration.days(7),
@@ -16,7 +16,7 @@ export class GitActionStack extends cdk.Stack {
       description: 'KMS key for encrypting the objects in an S3 bucket',
       enableKeyRotation: false,
     });
-
+*/
     const s3Bucket = new s3.Bucket(this, 'my-bucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       encryption: s3.BucketEncryption.KMS,
@@ -24,12 +24,6 @@ export class GitActionStack extends cdk.Stack {
       //encryptionKey: key,
     });
 
-    new cdk.CfnOutput(this, 'key-arn', {
-      value: key.keyArn,
-    });
 
-    new cdk.CfnOutput(this, 'bucket-name', {
-      value: s3Bucket.bucketName,
-    });
   } 
 }
